0c4a1079a2617fb86e03b1640a912b252b70412f
Jonathan Gallo <jonnygallo@gmail.com>
2018-09-10 16:10:34 -0700

    Import export demo complete

---
+++ app/math.js
@@ -0,0 +1,13 @@
+export function add(x,y){
+    return x+y;
+}
+
+export const subtract = (x,y) => {
+    return x-y;
+}
+
+export const multiply = (x,y) => x*y;
+
+export const divide = (x,y) => y ? x/y: 'Con\'t divide by zero';
+
+export default "this is the math default";
